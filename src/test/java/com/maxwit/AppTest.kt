c5eaff7468385f9aa3e1638c892219a052c75eb2
Conke Hu <conke.hu@maxwit.com>
2020-02-07 16:44:18 +0800

    init
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ src/test/java/com/maxwit/AppTest.kt
@@ -0,0 +1,17 @@
+package com.maxwit
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    fun shouldAnswerWithTrue() {
+        Assert.assertTrue(true)
+    }
+}
